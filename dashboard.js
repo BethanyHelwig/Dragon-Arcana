9ab8620b9bba2e05b680f48ae959fe16535794c4
PhoenixVerus <73077264+PhoenixVerus@users.noreply.github.com>
2025-08-21 21:58:20 -0500

    Updated with mobile first design

--- dashboard.js
+++ dashboard.js
@@ -4,51 +4,37 @@ const mainContent = document.getElementById('main-content')
 const charactersBtn = document.getElementById('characters-btn')
 const copyrightText = document.getElementById('copyright-text')
 
+// listener for button clicks
 document.addEventListener('click', function(e){
+    
     if(e.target.id === 'characters-btn'){
         console.log("Characters button clicked!")
     }
+    else if (e.target.dataset.btn){
+        if (!document.getElementById('nav-links').style.display){
+            document.getElementById('nav-links').style.display = 'none'
+        }
+        document.getElementById('nav-links').style.display = 
+            document.getElementById('nav-links').style.display === 'none' ? 'flex' : 'none';
+    }
 })
 
+// updates the copyright text to always show the current year
 copyrightText.innerText = `Copyright © ${new Date().getFullYear()} Phantom Fox`
 
+// renders out the content in the main section
 function renderDefaultState(userData) {
+    renderCharacters(userData)
+    renderGames(userData)
+    renderGreeting(userData)
+}
 
-    let contentHtml = `
-        <h1>Welcome, ${userData.username}!</h1>
+// renders the saved games section
+function renderGames(userData) {
 
-        <div class="flex">
-            <!-- CHARACTERS -->
-            <section class="main-content-section gradient-border" aria-label="Characters">
-                <h2>Characters</h2>
-        `
+    let contentHtml = '<h2>Games</h2>'
 
-    if (userData.characters){
-        userData.characters.forEach(function(character){
-            contentHtml += `
-                <div class="character">
-                    <h3>${character.name}</h3>
-                    <p>Class: ${character.class}</p>
-                </div>
-            `
-        })
-    }
-    else {
-        contentHtml += `
-            <p>You have no saved characters.</p>
-        `
-    }
-
-    contentHtml +=
-        `
-            </section>
-
-            <!-- GAMES -->
-            <section class="main-content-section gradient-border" aria-label="Games">
-                <h2>Games</h2>
-        `
-
-    if (userData.games){
+     if (userData.games){
         userData.games.forEach(function(game){
             contentHtml += `
                 <div class="game">
@@ -66,28 +52,40 @@ function renderDefaultState(userData) {
 
     contentHtml +=
         `
-                <div>
-                    <button>Join Game <i class="fa-solid fa-dice-d20"></i></button>
-                    <button>Create Game <i class="fa-solid fa-plus"></i></button>
-                </div>
-            </section>
-
-            <!-- WORLD OF D&D -->
-            <section class="main-content-section gradient-border" aria-label="World of D&D">
-                <h2>World of D&D</h2>
-                <form>
-                    <label for="dnd-search">Search D&D Database:</label>
-                    <input type="search" id="dnd-search" placeholder="What are you looking for?">
-                    <button type="submit">Go!</button>
-                </form>
-            </section>
+        <div>
+            <button>Join Game <i class="fa-solid fa-dice-d20"></i></button>
+            <button>Create Game <i class="fa-solid fa-plus"></i></button>
         </div>
+         `
+    document.getElementById('games-container').innerHTML = contentHtml
+}
+
+// renders the saved characters section
+function renderCharacters(userData) {
+    
+    let contentHtml = '<h2>Characters</h2>'
+
+    if (userData.characters){
+        userData.characters.forEach(function(character){
+            contentHtml += `
+                <div class="character">
+                    <h3>${character.name}</h3>
+                    <p>Class: ${character.class}</p>
+                </div>
+            `
+        })
+    }
+    else {
+        contentHtml += `
+            <p>You have no saved characters.</p>
         `
+    }
 
-    mainContent.innerHTML = contentHtml
+    document.getElementById('characters-container').innerHTML = contentHtml
+}
 
-    console.log(contentHtml)
-
+function renderGreeting(userData){
+    document.getElementById('user-welcome').textContent = `Welcome, ${userData.username}!`
 }
 
 renderDefaultState(userSavedData)
