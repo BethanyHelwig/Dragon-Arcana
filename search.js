ee6986eeeaa17529e5db5b8468b5fbd24d67fd5c
PhoenixVerus <73077264+PhoenixVerus@users.noreply.github.com>
2025-08-31 12:14:49 -0500

    Updates to search parameters and filtering added

--- search.js
+++ search.js
@@ -3,7 +3,7 @@ const searchResultsContainer = document.getElementById('search-results')
 
 document.addEventListener('submit', function(e) {
     e.preventDefault()
-    search()
+    search(document.querySelector('input[name="filter"]:checked').value)
 })
 
 document.addEventListener('click', function(e) {
@@ -17,16 +17,24 @@ document.addEventListener('click', function(e) {
     }
 })
 
-function search(){
+document.getElementById('search-dropdown').addEventListener('change', function(e) {
+    if (e.target.value === "monsters"){
+        console.log(e.target.value)
+        document.getElementById('search-filters').style.display = "flex"
+    }
+    else {
+        document.getElementById('search-filters').style.display = "none"
+    }
+})
+
+function search(filter){
     const searchTerm = document.getElementById('dnd-search').value
     const searchCategory = document.getElementById('search-dropdown').value
-    console.log(searchTerm)
-    console.log(searchCategory)
     
-    fetch(`https://www.dnd5eapi.co/api/2014/${searchCategory}/?name=${searchTerm}`)
+    console.log(`Fetching: https://www.dnd5eapi.co/api/2014/${searchCategory}/?${filter}=${searchTerm}`)
+    fetch(`https://www.dnd5eapi.co/api/2014/${searchCategory}/?${filter}=${searchTerm}`)
         .then(response => response.json())
         .then(data => {
-            console.log(data)
             if (data.count === 0){
                 searchResultsContainer.innerHTML = `
                     <p>No results.</p>
@@ -54,37 +62,52 @@ async function getDetails(url, id, category) {
     const response = await fetch(`https://www.dnd5eapi.co${url}`)
     const data = await response.json()
 
-    console.log(data)
-    if (category === "monsters") {
-        
-        const actions = data.actions.length > 0 
+    if (category === "monsters") {    
+        renderMonster(data, id, url)
+    }
+    else if (category === "alignments"){
+        renderAlignment(data, id)
+    }
+}
+
+function renderMonster(data, id, url) {
+    const actions = data.actions.length > 0 
             ? data.actions.map( action => action.name).join(", ") : "n/a"
 
-        document.getElementById(id).innerHTML += `
-            <div class="search-result-content">
-                <img src="https://www.dnd5eapi.co${data.image}" alt="Image of ${data.name}">
-                <ul>
-                    <li>Type: <span class="emphasis">${data.type}</span></li>
-                    <li>Alignment: <span class="emphasis">${data.alignment}</span></li>
-                    <li>Challenge Rating: <span class="emphasis">${data.challenge_rating}</span></li>
-                    <li>XP: <span class="emphasis">${data.xp}</span></li>
-                    <li>Ability Scores:
-                        <ul>
-                            <li>Charisma: <span class="emphasis">${data.charisma}</span></li>
-                            <li>Constitution: <span class="emphasis">${data.constitution}</span></li>
-                            <li>Dexterity: <span class="emphasis">${data.dexterity}</span></li>
-                            <li>Intelligence: <span class="emphasis">${data.intelligence}</span></li>
-                            <li>Strength: <span class="emphasis">${data.strength}</span></li>
-                            <li>Wisdom: <span class="emphasis">${data.wisdom}</span></li>
-                        </ul>
-                    </li>
-                    <li>Hit Points: <span class="emphasis">${data.hit_points} (${data.hit_points_roll})</span></li>
-                    <li>Hit Dice: <span class="emphasis">${data.hit_dice}</span></li>
-                    <li>Armor Class: <span class="emphasis">${data.armor_class[0].value} (${data.armor_class[0].type})</span></li>
-                    <li>Actions: <span class="emphasis">${actions}</span></li>
-                </ul>
-            </div>
-            <button data-sheeturl="${url}">Generate full character sheet</button>
-        `
-    }
+    document.getElementById(id).innerHTML += `
+        <div class="search-result-content">
+            <img src="https://www.dnd5eapi.co${data.image}" alt="Image of ${data.name}">
+            <ul>
+                <li>Type: <span class="emphasis">${data.type}</span></li>
+                <li>Alignment: <span class="emphasis">${data.alignment}</span></li>
+                <li>Challenge Rating: <span class="emphasis">${data.challenge_rating}</span></li>
+                <li>XP: <span class="emphasis">${data.xp}</span></li>
+                <li>Ability Scores:
+                    <ul class="ability-scores">
+                        <li class="ability-scores">Charisma: <span class="emphasis">${data.charisma}</span></li>
+                        <li class="ability-scores">Constitution: <span class="emphasis">${data.constitution}</span></li>
+                        <li class="ability-scores">Dexterity: <span class="emphasis">${data.dexterity}</span></li>
+                        <li class="ability-scores">Intelligence: <span class="emphasis">${data.intelligence}</span></li>
+                        <li class="ability-scores">Strength: <span class="emphasis">${data.strength}</span></li>
+                        <li class="ability-scores">Wisdom: <span class="emphasis">${data.wisdom}</span></li>
+                    </ul>
+                </li>
+                <li>Hit Points: <span class="emphasis">${data.hit_points} (${data.hit_points_roll})</span></li>
+                <li>Hit Dice: <span class="emphasis">${data.hit_dice}</span></li>
+                <li>Armor Class: <span class="emphasis">${data.armor_class[0].value} (${data.armor_class[0].type})</span></li>
+                <li>Actions: <span class="emphasis">${actions}</span></li>
+            </ul>
+        </div>
+        <button data-sheeturl="${url}">Generate full character sheet</button>
+    `
+}
+
+function renderAlignment(data, id){
+    document.getElementById(id).innerHTML += `
+        <div class="search-result-content">
+            <ul>
+                <li>Abbreviation: <span class="emphasis">${data.abbreviation}</span></li>
+                <li>Description: <span class="emphasis">${data.desc}</span></li>
+            </ul>
+    `
 }
